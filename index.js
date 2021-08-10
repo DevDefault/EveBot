e563d0c0ffda56ca1f6f409413c75ee2de888706
carlostingu <tingu.contato@gmail.com>
2021-08-10 11:23:17 -0300

    Start EveBot

---
+++ index.js
@@ -0,0 +1,31 @@
+/* Config .env */
+require('dotenv').config()
+
+/* Express */
+const express = require('express')
+const app = express()
+const port = 3000
+
+/* Routes */
+const home = require("./app/home")
+
+/* Set Global Config */
+global.year = (new Date()).getFullYear()
+
+/* Assets */
+app.use('/assets', express.static(__dirname + '/theme/assets'));
+
+/* Template Egine */
+app.set('view engine', 'ejs')
+app.set("views", "theme/views")
+
+/* Parsing form */
+app.use(express.json())
+app.use(express.urlencoded({ extended: true })) 
+
+/* Start Routes */
+app.use('/', home)
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+})
