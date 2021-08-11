806295552a4d9f0588a094d2253da09b9569b780
carlostingu <tingu.contato@gmail.com>
2021-08-11 10:26:05 -0300

    update session

--- app/signin.js
+++ app/signin.js
@@ -1,10 +1,10 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
 
 /* Models */
 const User = require("../models/user")
 
-router.get('/', async function (req, res) {
+router.get("/", async function (req, res) {
     req.session.destroy()
 
     global.isLogged = false
@@ -12,7 +12,7 @@ router.get('/', async function (req, res) {
     res.render("signin")
 })
 
-router.post('/signin', global.multer().none(), async function (req, res) {
+router.post("/signin", global.multer().none(), async function (req, res) {
     var username = req.body.username
     var password = req.body.password
 
@@ -33,7 +33,7 @@ router.post('/signin', global.multer().none(), async function (req, res) {
             }))
         } else {
             res.send(JSON.stringify({
-                message: "error"
+                message: "fail"
             }))
         }
     } else {
@@ -43,7 +43,7 @@ router.post('/signin', global.multer().none(), async function (req, res) {
     }
 })
 
-router.get('/signout', async function (req, res) {
+router.get("/signout", async function (req, res) {
     req.session.destroy()
 
     global.isLogged = false
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /* Config .env */
-require('dotenv').config()
+require("dotenv").config()
 
 /* Express */
-const express = require('express')
+const express = require("express")
 const app = express()
 const port = 3000
 
-const session = require('express-session')
+const session = require("express-session")
 
-global.multer = require('multer')
+global.multer = require("multer")
 
 /* Routes */
 const signin = require("./app/signin")
@@ -18,27 +18,35 @@ const dashboard = require("./app/dashboard")
 global.year = (new Date()).getFullYear()
 
 /* Assets */
-app.use('/assets', express.static(__dirname + '/theme/assets'));
+app.use("/assets", express.static(__dirname + "/theme/assets"))
 
 /* Session */
-app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }})) 
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
 
 /* Template Egine */
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs")
 app.set("views", "theme/views")
 
 /* Parsing form */
 app.use(express.json())
-app.use(express.urlencoded({ extended: true })) 
+app.use(express.urlencoded({
+    extended: true
+}))
 
 /* Start Routes */
-app.use('/', signin)
+app.use("/", signin)
 app.use("/dashboard", dashboard)
 
-app.get('*', function(req, res){
-    res.status(400).render("not_found", { statusCode: res.statusCode })
+app.get('*', function (req, res) {
+    res.status(400).render("not_found", {
+        statusCode: res.statusCode
+    })
 })
-  
+
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
 })
