7e9141d73130599bf516587d4ac8331dde4f73bf
Default <52261814+DevDefault@users.noreply.github.com>
2021-08-14 20:07:36 -0300

    update bot

--- app/signin.js
+++ app/signin.js
@@ -5,28 +5,6 @@ module.exports = function(app, io) {
     app.get("/", (req, res) => {
         io.on('connection', (socket) => {
             socket.on("signin_request", (form) => {
-<<<<<<< HEAD
-                User.findOne({
-                    where: {
-                        username: form.username,
-                        password: form.password
-                    }
-                }).then((user) => {
-                    var message;
-                    if (user) {
-                        req.session.user = user
-
-                        global.isLogged = true
-
-                        message = "success"
-                    } else {
-                        message = "error"
-                    }
-
-                    socket.emit("signin_response", message)
-                })
-            })
-=======
                 var message;
 
                 if (form.username != "" && form.password != "") {
@@ -54,7 +32,6 @@ module.exports = function(app, io) {
                 }
             })
             
->>>>>>> 39aee3551220e544014c9c854e705a02ed6cb4bf
             // socket.on('disconnect', () => {
             //     console.log('user disconnected')
             // })
